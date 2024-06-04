d9f8ae3841ddb61f6c4463ba37d5f62364952cee
Esteban Solís <estebansolis829@hotmail.com>
2024-06-03 18:31:56 -0600

    INICIO DE SESION Y REGISTRO

--- ProyectoG6/Controllers/HomeController.cs
+++ ProyectoG6/Controllers/HomeController.cs
@@ -13,18 +13,10 @@ namespace ProyectoG6.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Registro()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
