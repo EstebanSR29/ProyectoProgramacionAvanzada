4e996e061a348fe85210b1c05bf5ee48f97c63e5
BryanOR96 <bryorozco@gmail.com>
2024-06-23 10:01:28 -0600

    entidades

--- ProyectoG6/Controllers/HomeController.cs
+++ ProyectoG6/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectoG6.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,16 +9,33 @@ namespace ProyectoG6.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Index(usuario user)
+        {
+            //validar credenciales para verificar si avanza o no 
+            return View();
+        }
+
+        [HttpGet]
         public ActionResult Registro()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Registro(usuario user)
+        {
+            return View();
+        }
+
+
+        [HttpGet]
         public ActionResult Home()
         {
             return View();
---
+++ ProyectoG6/Entidades/usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoG6.Entidades
+{
+    public class usuario
+    {
+        public string correo {  get; set; }
+        public string Nombre { get; set; } 
+        public string contrasena {  get; set; }
+
+
+
+    }
+}
